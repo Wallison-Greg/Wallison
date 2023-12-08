02bcac5e3f9b75acd0f913ce235adc0eaf50aef3
wallison da silva gregorio <wallisongregorio2022@gmail.com>
2023-12-08 16:54:52 -0300

    inicio do menu dropdown de login e contato

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,11 @@ import './Header.css'
 import { NavLink } from 'react-router-dom'
 import Logo from '../assets/img/logo_2.png'
 
+//icons 
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+
 const Header = ({black}) => {
 
     const [active, seActive] = useState(false);
@@ -42,8 +47,39 @@ const Header = ({black}) => {
                     <li>
                         <NavLink to='/projects'>Novo Projeto</NavLink>
                     </li>
-                    <li>
-                        <NavLink to='#'>Contato</NavLink>
+                    <li className='dropdown'>
+                        <div class="menu-dropdown">
+                            <div className='title-dropdown'>Contato</div>
+                            <div className='perfil-dropdown'></div>
+                        </div>
+                        <div className='sub-menu'>
+                            <div class="acount">
+                                <h1 style={{textAlign: 'center'}}>Contato</h1>
+                                <div className='contact'>
+                                    <a className='icon' href='https://github.com/Wallison-Greg' target="_blank" rel="noopener noreferrer">
+                                        <GitHubIcon />
+                                    </a>
+                                    <a className='icon' href='https://wa.me/5562999769017' target="_blank" rel="noopener noreferrer">
+                                        <WhatsAppIcon />
+                                    </a>
+                                    <a className='icon' href='https://www.linkedin.com/in/wallison-gregorio-ba23a922b/' target="_blank" rel="noopener noreferrer">
+                                        <LinkedInIcon />
+                                    </a>
+                                </div>
+                                <hr style={{width: 230}}/>
+                                <form className='login'>
+                                    <h1 style={{textAlign: 'center'}}>Login</h1>
+                                    <label>
+                                        <span>E-mail:</span>
+                                        <input type='text' placeholder='E-mail' required name='email' />
+                                    </label>
+                                    <label>
+                                        <span>Senha:</span>
+                                        <input type='text' placeholder='Senha' required name='password' />
+                                    </label>
+                                </form>
+                            </div>
+                        </div>
                     </li>
                 </ul>
             </nav>
@@ -53,3 +89,7 @@ const Header = ({black}) => {
 }
 
 export default Header
+
+/*
+<NavLink to='#'>Contato</NavLink>
+*/
